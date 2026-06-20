a22eff04a66124d2db90a295c4c513e92946dfa2
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-06-19 22:02:32 -0400

    Agregando break

--- script.js
+++ script.js
@@ -24,39 +24,10 @@ do {
         alert(`La suma de ventas es ${sumaVentas}. Promedio de ventas es ${promVentas}`)
     }else if(introIngreso =="2"){
         alert(`No se han ingresado datos`);
-        console.log("No se han ingresado datos");       
+        console.log("No se han ingresado datos"); 
+        break;      
     }else{
-        alert(`Dato no válido`);
+        console.log("Dato no válido");
         break;
     }
 }while (!cerrar)
-
-
-
-
-
-
-let ventas = [
-  [55000, 74000, 60500, 60000, 80000],   // semana 1
-  [67000, 58500, 90000, 76500, 60000]    // semana 2
-];
-let total = 0;
-
-for (let nroSemana = 0; nroSemana < ventas.length; nroSemana++) {
-       
-    const semana = ventas[nroSemana];
-    console.log(`semana: ${nroSemana+1} (indice:${nroSemana})`);
-    
-    for (let nroDia = 0; nroDia < semana.length; nroDia++) {
-        
-        const dia = semana[nroDia];
-       // console.log(`dia: ${nroDia+1} (indice: ${nroDia}) = ${dia}`);
-        console.log(`dia: ${nroDia+1} (indice: ${nroDia}) = ${ventas[nroSemana][nroDia]}`);
-
-       // total+= dia;
-
-        total += ventas[nroSemana][nroDia]
-    }
-}
-
-//console.log("Total Vendido",total);
